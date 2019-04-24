6600cdcc47bc343c2208c4f7efb5ad46fd266db4
Sanjay Sai <shachsan@hotmail.com>
2019-04-24 11:40:32 -0400

    add functionality to allow user to create limited number of decks

--- models/deckCollection.js
+++ models/deckCollection.js
@@ -1,26 +1,31 @@
 class DeckCollection{
     constructor(){
         this.decks=[];
+        this.maxDeck=5;
     }
 
     createNewDeck(){
         console.log('=====new deck created===');
-        const deck={id:this.decks.length+1}
-        const cards=[];
+        if(this.decks.length===this.maxDeck){
+            return {message:`You can only have ${this.maxDeck} decks at a time`}
+        } else{
+            const deck={id:this.decks.length+1}
+            const cards=[];
 
-        const values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
-        const suits = ['spades', 'club', 'heart', 'diamond']
+            const values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+            const suits = ['spades', 'club', 'heart', 'diamond']
 
-        for(let v=0;v<values.length;v++){
-            for(let s=0;s<suits.length;s++){
-                cards.push({value:values[v], suit:suits[s]});
+            for(let v=0;v<values.length;v++){
+                for(let s=0;s<suits.length;s++){
+                    cards.push({value:values[v], suit:suits[s]});
+                }
             }
-        }
-        deck.cards = cards;
-        deck.remaining=cards.length;
-        this.decks.push(deck);
+            deck.cards = cards;
+            deck.remaining=cards.length;
+            this.decks.push(deck);
 
         return deck;
+        }
 
     }
 
